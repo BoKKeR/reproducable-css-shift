7b35fb55d20f42af15fdfd8f666bb89ee4a06574
Philip Rutberg <philip.rutberg@postnord.com>
2024-08-01 15:53:45 +0200

    ModuleLayout imported from ap package

--- iam-ap-access-list/src/layouts/DefaultLayout.vue
+++ iam-ap-access-list/src/layouts/DefaultLayout.vue
@@ -1,27 +1,24 @@
 <script setup lang="ts">
 import { defineEmits } from 'vue';
 
+import ModuleLayout from './ModuleLayout.vue';
+
 defineEmits(['create-access-list']);
 </script>
 
 <template>
-  <div class="container">
-    <div class="top">
-      <header>
-        <div class="text">
-          <h1>Access list manager</h1>
-          <h2>
-            The Access List Manager allows configuration of permissions for
-            different user groups across new or existing systems.
-          </h2>
-        </div>
-        <button
-          class="create-access-list-button"
-          @click="$emit('create-access-list')"
-        >
-          Create access list
-        </button>
-      </header>
+  <ModuleLayout>
+    <template #title>Access list manager</template>
+    <template #subtitle>
+      The Access List Manager allows configuration of permissions for different
+      user groups across new or existing systems.
+    </template>
+    <template #action>
+      <button class="create-button" @click="$emit('create-access-list')">
+        Create access list
+      </button>
+    </template>
+    <template #search>
       <div class="search-container">
         <input
           class="access-list-search"
@@ -32,65 +29,17 @@ defineEmits(['create-access-list']);
           To search for users go to <a href="/manage-user">Manage user</a>
         </p>
       </div>
-    </div>
-    <div class="content">
-      <h4>No access lists found</h4>
-      <p>Create a new access list to get started.</p>
-    </div>
-  </div>
+    </template>
+    <template #content>
+      <div class="content">
+        <h4>No access lists found</h4>
+        <p>Create a new access list to get started.</p>
+      </div>
+    </template>
+  </ModuleLayout>
 </template>
 
 <style lang="scss" scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  gap: 4rem;
-}
-
-.top {
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-}
-
-header {
-  display: flex;
-  gap: 2rem;
-}
-
-.text {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-
-  h1 {
-    font-weight: 700;
-    font-size: 2rem;
-  }
-
-  h2 {
-    font-weight: 400;
-    font-size: 1rem;
-  }
-
-  .create-access-list-button {
-    background-color: blue;
-    color: gray;
-    border: none;
-    padding: 0.5rem 1rem;
-    border-radius: 0.5rem;
-    cursor: pointer;
-  }
-}
-
-.text-area-search {
-  width: 100%;
-  height: 2rem;
-  padding: 0.5rem;
-  border-radius: 0.5rem;
-  border: 1px solid gray;
-}
-
 .search-container {
   display: flex;
   flex-direction: column;
@@ -122,6 +71,21 @@ header {
 
   p {
     font-weight: 400;
+  }
+}
+
+.create-button {
+  background-color: #0070d2;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  font-size: 1rem;
+  font-weight: 700;
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: #005bb5;
   }
 }
 </style>
---
+++ iam-ap-access-list/src/layouts/ModuleLayout.vue
@@ -0,0 +1,68 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="module-layout">
+    <div class="header">
+      <div class="info">
+        <h1>
+          <slot name="title" />
+        </h1>
+        <p>
+          <slot name="subtitle" />
+        </p>
+      </div>
+      <div class="action">
+        <slot name="action" />
+      </div>
+    </div>
+    <div class="search">
+      <slot name="search" />
+    </div>
+    <div class="module-layout-content">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.module-layout {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  gap: 5rem;
+
+  h1 {
+    font-size: 2rem;
+  }
+  p {
+    color: gray;
+  }
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    gap: 32px;
+    align-items: center;
+
+    .info {
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+    }
+  }
+
+  .search {
+    width: 100%;
+  }
+
+  .action {
+    min-width: max-content;
+  }
+
+  .module-layout-content {
+    flex: 1;
+    overflow-y: auto;
+  }
+}
+</style>
